d78e429bc96aa4c7d2a6119571d50bdd30abeeb8
ravail121 <rvlirshad@gmail.com>
2022-03-09 12:33:40 +0500

    aa

---
+++ app/Http/ViewComposer/CartComposer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\ViewComposer;
+
+use Illuminate\View\View;
+use App\Services\Cart\CartResponse;
+
+class CartComposer
+{
+    protected $cart;
+
+    public function __construct(CartResponse $response)
+    {
+        $this->cart = $response;
+    }
+
+    public function compose(View $view)
+    {
+        //dd(session('cart'));
+        $view->with([
+            'totalPrice'    => session('cart')['totalPrice'],
+            'subtotalPrice' => session('cart')['subtotalPrice'],
+            'activeGroupId' => session('cart')['activeGroupId'],
+            'monthlyCharge' => session('cart')['monthlyCharge'],
+            'taxes'         => session('cart')['taxes'],
+            'regulatory'    => session('cart')['regulatory'],
+            'shippingFee'   => session('cart')['shippingFee'],
+            'coupons'       => isset($this->cart->coupon()['total']) ? $this->cart->coupon()['total'] : 0,
+        ]);
+      //  dd(2);
+    }
+
+}
---
+++ resources/views/static-pages/features.blade.php
@@ -0,0 +1,20 @@
+<html>
+	<body>
+
+	<div class="mobile_feature" style="background:#f6f5f5;">
+	<section class="phonesFeaturesHero">
+		<div class="wrapper">
+			<h1>Mobilizing your World</h1>
+			<p>No rate increases. No annual service contracts. No data overages. No limits. NO contract.</p>
+			<!---<a href="plans.php" style="font-size: 20px;" class="button-custom bluu">starts at $20/<span>mo.</span> <span class="fa fa-chevron-right"></span></a> --->
+			<img src="{{ asset('theme/newstyle/img/84_New-Project.jpg') }}" alt="hero" style="width:100%;">
+		</div>
+
+	</section>
+	</div>
+	</body>
+</html>
+
+    <!-- ======= Hero Section ======= -->
+	
+
